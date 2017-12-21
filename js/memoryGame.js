36571637a59d018ad5820d58218c85acd296a603
benRoz <bensroz@gmail.com>
2017-12-22 01:26:31 +0200

    changing diffucalty is working

--- js/memoryGame.js
+++ js/memoryGame.js
@@ -1,4 +1,4 @@
-var template = document.getElementsByClassName("container")[0].innerHTML;
+var oneRow = document.getElementsByClassName("container")[0].innerHTML;
 var container = document.getElementsByClassName("container")[0];
 
 var difficulty = {
@@ -6,21 +6,43 @@ var difficulty = {
     medium : 3,
     hard : 4
 }
+var difficultLevel=2;
+var pic= ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','0.jpg', '0.jpg'];
+
 
 var addRow = function(num){
+    container.innerHTML = oneRow;
     for(var i = 0; i < num; i++){
-        container.innerHTML = container.innerHTML + template;
-        console.log(container.innerHTML);
+       container.innerHTML = container.innerHTML + oneRow;
     }
 }
 addRow(difficulty.easy);
 
-var pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','0.jpg', '0.jpg'];
-if (difficulty.easy==3){
-    pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','6.jpg', '6.jpg', '7.jpg', '7.jpg', '0.jgp','0.jpg',];
-}
-else if (difficulty.easy==4){
-    pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5,jpg', '5.jpg','6.jpg', '6.jpg', '7.jpg', '7.jpg', '8.jgp','8.jpg','9.jpg', '9.jpg','0.jpg','0.jpg'];
+
+function difficultChangeParameterss(){
+var select= document.getElementById("select").value;
+    if (select=="3"){
+        addRow(difficulty.medium);
+        difficultLevel=3;
+    }
+    else if (select=="4"){
+        addRow(difficulty.hard);
+         difficultLevel=4;
+    }
+     else if (select=="2"){
+        addRow(difficulty.easy);
+        difficultLevel=2;
+    }
+
+    if(difficultLevel==2){
+       pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','0.jpg', '0.jpg'];
+    }
+    else if (difficultLevel==3){
+        pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','6.jpg', '6.jpg', '7.jpg', '7.jpg', '0.jpg','0.jpg',];
+    }
+    else if (difficultLevel==4){
+        pic = ['1.jpg','1.jpg', '2.jpg', '2.jpg' , '3.jpg', '3.jpg','4.jpg', '4.jpg', '5.jpg', '5.jpg','6.jpg', '6.jpg', '7.jpg', '7.jpg', '8.jpg','8.jpg','9.jpg', '9.jpg','0.jpg','0.jpg'];
+    }
 }
 
 function shuffleArray(array) {
@@ -33,13 +55,16 @@ function shuffleArray(array) {
 }
 shuffleArray(pic);
 
-
-var allCards= document.getElementsByClassName('card');
-for (var x=0; x<allCards.length; x++){
-    allCards[x].id= "card"+ x;
-    allCards[x].addEventListener("click", flipCards);
-    allCards[x].style.backgroundImage="url('./images/back6.jpg')";
+var allCards;
+function assigningIdAndImageAndEvent(){
+    allCards= document.getElementsByClassName('card');
+    for (var x=0; x<allCards.length; x++){
+        allCards[x].id= "card"+ x;
+        allCards[x].addEventListener("click", flipCards);
+        allCards[x].style.backgroundImage="url('./images/back6.jpg')";
+    }
 }
+assigningIdAndImageAndEvent();
 
 var counter=0;
 var firstFlip={};
@@ -47,47 +72,60 @@ var locat=0;
 var secondFlip={};
 var correctAnswer=0;
 var mistake=0;
-function flipCards(){
 
+function flipCards(){
     counter+=1
     var cardId = this.id;
     var take = cardId.split("card");
     locat=take[1];
-    this.style.backgroundImage= "url('./images/"+pic[locat]+"')" ;
 
     if (counter==1){
+        this.style.backgroundImage= "url('./images/"+pic[locat]+"')" ;
         firstFlip.url=this.style.backgroundImage;
         firstFlip.locat = locat;
         firstFlip.id = cardId;
     }
     else if (counter==2){
+        this.style.backgroundImage= "url('./images/"+pic[locat]+"')" ;
         secondFlip.url= this.style.backgroundImage;
         secondFlip.locat = locat;
         secondFlip.id = cardId;
 
         setTimeout(function(){
             if (secondFlip.url!=firstFlip.url){
-                console.log("printingonly if no match");
                 document.getElementById(firstFlip.id).style.backgroundImage = "url('./images/back6.jpg')";
                 document.getElementById(secondFlip.id).style.backgroundImage = "url('./images/back6.jpg')";
                 mistake=mistake+2;
             }
             else if (secondFlip.url==firstFlip.url){
                 correctAnswer=correctAnswer+2;
-
             }
 
-            counter=0;
-            if (correctAnswer==2){
+            if (correctAnswer==12){
                 console.log("correct");
                 alert("you win you had "+mistake+" mistakes ");
             }
-
+        counter=0;
         }, 900);
     }
+}
+document.getElementById("newGame").addEventListener("click",newGame)
+function newGame(){
+    counter=0;
+    firstFlip={};
+    locat=0;
+    secondFlip={};
+    correctAnswer=0;
+    mistake=0;
+    for (var x=0; x<allCards.length; x++){
+         allCards[x].style.backgroundImage="url('./images/back6.jpg')";
 
-
-
-        }
-
-
+    }
+}
+document.getElementById("select").addEventListener("click", changingGameLevel);
+function changingGameLevel(){
+    difficultChangeParameterss();
+    shuffleArray(pic);
+    assigningIdAndImageAndEvent();
+    newGame();
+}
